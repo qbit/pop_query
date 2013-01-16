ca8df1b6f300a7cc9ffcfa638acb23433e0335aa
Aaron Bieber <deftly@gmail.com>
2013-01-15 18:20:50 -0700

    pew

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 
 var http = require( 'http' ),
-pg = require( 'pg' );
+pg = require( 'pg' ),
+server;
+
+server = http.createServer( function( req, res ) {
+  var data = [];
+
+  req.on( 'data', function( chunk ) {
+    data.push( chunk );
+  });
+
+  req.on( 'end', function() {
+    console.log( data.join( '' ).toString() );
+
+    // pass the majobber back to postgres and do an ST_Contains kinda
+    // query - pass back awesomesauce
+    
+    res.writeHead( 200, { 'Content-Type': 'applicatoin/json' } );
+    res.end( JSON.stringify({}) );
+  });
+}).listen( 3030 );
